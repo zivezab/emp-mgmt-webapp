de97c447e6298f2a9cacdc3c5bbcd60d98df63c7
Zive Lai <zive.lai@gmail.com>
2020-11-10 09:09:22 +0800

    USER STORY 1: update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ RUN pip install -r requirements.txt
 RUN apt-get update && apt-get install -y gettext
 COPY entrypoint.sh /code/
 COPY . /code/
+RUN chmod a+x /code/entrypoint.sh
 ENTRYPOINT ["/code/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ then
     echo "PostgreSQL started"
 fi
 
-#python manage.py flush --no-input
-python manage.py migrate
 python manage.py compilemessages
+#python manage.py flush --no-input
+python manage.py migrate --noinput
 
 exec "$@"
